c9907bdfe9de33d09c9c712d66205370bc948029
SethMulti <sorrelsseth@gmail.com>
2018-11-27 13:28:52 -0500

    Added site files

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Seth Sorrels' Webpage</title>
+<link rel='stylesheet' type='text/css' href='css.css'>
+<meta charset="utf-8"/>
+</head>
+<div id="background">
+<div id="container">
+
+<div id="titlebar">
+<img src="/Pics/titlebar.jpg">
+</div>
+
+<div id="logo">
+<img src="/Pics/logo.png" class="picright" width="490" height="65">
+</div>
+
+<div id="sidepanel">
+
+<h2 style="margin:0px" class="blue">Links:</h2>
+
+<ul class="green" style="list-style: none; padding-left: 0px; margin-right:90px">
+<li class><a href="https://www.instagram.com/seth.sorrels/" class="green">Instagram</a></li>
+<li><a target="_blank" href="https://twitter.com/_Earl_Powell_" class="green">Twitter</a></li>
+<li><a href="https://www.goodreads.com/user/show/84619940-seth" class="green">Muh books</a></li>
+<li><a href="https://www.chess.com/member/earlpowell" class="green">Play me in chess</a></li>
+<li><a target="_blank" href="https://github.com/SethMulti" class="green">Github</a></li>
+<li><a target="_blank" href="http://sorrels.us" class="green">Other/old website</a></li>
+<li><a href="http://www.sorrels.us/homepage_v2/" class="green">homepage(old)</a></li>
+<li><a href="http://lukesmith.xyz/" class="green">(The guy who inspired this website.)</a>
+</ul>
+<img src="/Pics/pepe.jpg" width="200" height="175">
+</div>
+
+<div id="content">
+
+<h2 style="margin:0px" class="blue">Welcome Comrade!</h2>
+
+<h3><small><p class="red">
+This site was made so I could play my part in letting the CIA and FBI spy on me.
+</p>
+
+<h2><small><p class="blue">Things that I'm know for:</p></small></h2>
+<ul class="blue"><h2><small>
+<li>Being an enthused pseudointellectual.</li>
+<li>Someone who knows a thing or two about computers</li>
+<li>Someone who reads a lot, despite reading painfully slow.</li>
+<li>A fan of <a href="https://jordanbpererson.com/" class="green">Jordan B. Peterson</a>, <a href="https://en.wikipedia.org/wiki/Carl_Jung" class="green">Carl G. Jung</a>, and <a href="https://en.wikipedia.org/wiki/Friedrich_Nietzsche" class="green">Fredrick Neichze.</a></li>
+<li>The guy runing this dumpy site.</li>
+<li>Having the "True Neutral" DnD aliment.</li>
+<li>Someone who is completely indecisive, regarding politics.</li>
+</h2><small></ul>
+
+</div>
+
+
+</div>
+</html>
---
+++ old/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Seth Sorrels' Webpage</title>
+<link rel='stylesheet' type='text/css' href='style.css'>
+<meta charset="utf-8"/>
+</head>
+
+<body> 
+
+<div id="title">
+<h1><big>Seth Sorrels</big> <small>(http://sethpage.info/)</small></h1>
+</div>
+
+<h2 style="margin:0px">Welcome Comrade!</h2>
+
+<h3><small><p>
+This site was made so I could play my part in letting the CIA and FBI spy on me.
+</p>
+
+<div id="demoObject">
+<h2><small><p>Things that I'm know for:</p></small></h2>
+<ul><h2><small>
+<li>Being an enthused pseudointellectual.</li>
+<li>Someone who knows a thing or two about computer
+<li>Someone who reads a lot, despite reading painfully slow.</li>
+<li>A fan of <a href="https://jordanbpererson.com/">Jordan B. Peterson</a>, <a href="https://en.wikipedia.org/wiki/Carl_Jung">Carl G. Jung</a>, and <a href="https://en.wikipedia.org/wiki/Friedrich_Nietzsche">Fredrick Neichze.</a></li>
+<li>The guy runing this dumpy site.</li>
+<li>Having the "True Neutral" DnD aliment.</li>
+<li>Someone who is completely indecisive regarding politics.</li>
+</h2><small></ul></div>
+
+
+
+</html>
